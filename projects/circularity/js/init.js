2934141a66e796f5e603054ef46722266096168d
Justin ahiagbede <jthemoneymaker123@gmail.com>
2023-03-05 21:30:10 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,10 +20,14 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+             var circle;
+             //Mutiple circles//
+             var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
 
